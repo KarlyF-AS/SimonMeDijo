c4e24efd5749c6207bd5a3463456c690a856d236
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2025-11-12 11:40:24 +0100

    Clase datos arreglada

--- app/src/main/java/com/dam/simonmedijo/Datos.kt
+++ app/src/main/java/com/dam/simonmedijo/Datos.kt
@@ -1,27 +1,26 @@
 package com.dam.simonmedijo
 
 import androidx.compose.ui.graphics.Color
+import kotlinx.coroutines.flow.MutableStateFlow
 
 
-class Datos {
-    object Datos {
-        var ronda = 0
-        var secuencia = mutableListOf<Int>()
-        var secuenciaUsuario = mutableListOf<Int>()
-        var record = 0
-        var estado = Estado.IDLE
-    }
+object Datos {
+        var ronda = MutableStateFlow(0)
+        var record = MutableStateFlow(0)
+        var estado = MutableStateFlow(Estado.IDLE)
+        var secuencia = MutableStateFlow(mutableListOf<Colores>())
+}
 
     /**
      * Enum con los estados del juego
      *
      */
 
-    enum class Estado {
-        IDLE,
-        GENERAR_SECUENCIA,
-        ELECCION_USUARIO,
-        FINALIZADO
+    enum class Estado(val botoneraIsActive: Boolean, val botonStartIsActive: Boolean) {
+        IDLE( botoneraIsActive = false, botonStartIsActive = true ),
+        GENERAR_SECUENCIA( botoneraIsActive = false, botonStartIsActive = false),
+        ELECCION_USUARIO( botoneraIsActive = true, botonStartIsActive = false),
+        FINALIZADO( botoneraIsActive = false, botonStartIsActive = true)
     }
 
     /**
@@ -29,10 +28,9 @@ class Datos {
      */
 
     enum class Colores(val color: Color, val txt: String) {
-        CLASE_ROJO(color = Color.Red, txt = "roxo"),
-        CLASE_VERDE(color = Color.Green, txt = "verde"),
-        CLASE_AZUL(color = Color.Blue, txt = "azul"),
-        CLASE_MORADO(color = Color.Yellow, txt = "melo")
+        CLASE_ROJO(color = Color.Red, txt = "Rojo"),
+        CLASE_VERDE(color = Color.Green, txt = "Verde"),
+        CLASE_AZUL(color = Color.Blue, txt = "Azul"),
+        CLASE_MORADO(color = Color.Magenta, txt = "Mora")
     }
 
-}
