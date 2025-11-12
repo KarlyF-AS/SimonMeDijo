fe037f44e707d8a72b96ec10e0f3fbbc1003a4e8
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2025-11-12 13:54:13 +0100

    Empezando clase view model

--- app/src/main/java/com/dam/simonmedijo/MyVM.kt
+++ app/src/main/java/com/dam/simonmedijo/MyVM.kt
@@ -1,4 +1,15 @@
 package com.dam.simonmedijo
 
-class MyVM {
+import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.ViewModel
+
+class MyVM : ViewModel(){
+    fun comprobarEleccionEnSecuencia(color: Colores, numeroSecuencia: Int):Boolean{
+        if(Datos.secuencia.value[numeroSecuencia] == color){
+            return true
+        }
+        return false
+    }
+
+
 }
