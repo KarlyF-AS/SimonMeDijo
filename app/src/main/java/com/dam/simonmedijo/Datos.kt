fbf12d1ab627ddf654bd3ec60346120325f4fffe
Daniel Figueroa <dfigueroavidal@danielcastelao.org>
2025-11-26 10:55:52 +0100

    Código comentado (#14)
    * clase MyVM completada
    
    * clase Datos comentada y eliminación una función innecesario
    
    * Todo comentado

--- app/src/main/java/com/dam/simonmedijo/Datos.kt
+++ app/src/main/java/com/dam/simonmedijo/Datos.kt
@@ -5,21 +5,26 @@ import kotlinx.coroutines.flow.MutableStateFlow
 
 
 object Datos {
-        var ronda = MutableStateFlow(0)
-        var record = MutableStateFlow(0)
-        var estado = MutableStateFlow(Estado.IDLE)
-        var secuencia = MutableStateFlow(mutableListOf<Colores>())
 
-        var currentColorEncendido: MutableStateFlow<Colores?> = MutableStateFlow(null)
+        var ronda = MutableStateFlow(0) // La ronda actual del juego
+        var record = MutableStateFlow(0) // El record persistente del juego
+        var estado = MutableStateFlow(Estado.IDLE) // El estado del juego
+        var secuencia = MutableStateFlow(mutableListOf<Colores>()) //La lista de colores que hay en cada ronda
+
+        var currentColorEncendido: MutableStateFlow<Colores?> = MutableStateFlow(null) // El color que se va a encender en cada fase de la ronda
+
 
 }
 
-    /**
-     * Enum con los estados del juego
-     *
-     */
+/**
+ *  Estados del juego, para el manejo de botones y texto del panel
+ *  @author Daniel Figueroa Vidal
+ *  @param botoneraIsActive Boolean
+ *  @param botonStartIsActive Boolean
+ *  @param textoPanel String
+ */
 
-    enum class Estado(val botoneraIsActive: Boolean, val botonStartIsActive: Boolean, val textoPanel: String) {
+enum class Estado(val botoneraIsActive: Boolean, val botonStartIsActive: Boolean, val textoPanel: String) {
         IDLE( botoneraIsActive = false, botonStartIsActive = true, textoPanel = "Presiona Start para comenzar" ),
         GENERAR_SECUENCIA( botoneraIsActive = false, botonStartIsActive = false, textoPanel = "Generando secuencia"),
         ELECCION_USUARIO( botoneraIsActive = true, botonStartIsActive = false, textoPanel = "Adivina la secuencia"),
@@ -28,22 +33,15 @@ object Datos {
 
     /**
      * Colores utilizados
+     * @author Daniel Figueroa Vidal
+     * @param color Color
+     * @param txt String
      */
-
     enum class Colores(val color: Color, val txt: String) {
         CLASE_ROJO(color = Color.Red, txt = "Rojo"),
         CLASE_VERDE(color = Color.Green, txt = "Verde"),
         CLASE_AZUL(color = Color.Blue, txt = "Azul"),
-        CLASE_MORADO(color = Color.Magenta, txt = "Mora");
-        fun pasarColorANumero(): Int {
-            return when (this) {
-                CLASE_ROJO -> 0
-                CLASE_VERDE -> 1
-                CLASE_AZUL -> 2
-                CLASE_MORADO -> 3
-                else -> -1
-            }
-        }
+        CLASE_MORADO(color = Color.Magenta, txt = "Mora")
     }
 
 
